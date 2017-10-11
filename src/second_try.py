84c56662e08e36610c38ce3e04ed28c5d32bace4
Harshvardhan Kalra <harshvardhan14043@iiitd.ac.in>
2017-10-11 10:59:31 +0530

    Added CNN model

--- src/second_try.py
+++ src/second_try.py
@@ -1,7 +1,7 @@
 # Reference : https://keon.io/deep-q-learning/
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
+from keras.layers import Dense, Conv2D, Dropout, MaxPooling2D, Flatten
 from keras.optimizers import Adadelta, Adam
 
 import gym
@@ -27,11 +27,15 @@ class Agent:
 
 	def get_model(self, input_shape, nb_actions):
 		model = Sequential()
-		model.add(Flatten(input_shape = input_shape))
+		model.add(Conv2D(16, (3, 3), input_shape = (1,) + input_shape, activation = 'relu',\
+			data_format = 'channels_first'))
+		model.add(MaxPooling2D(pool_size = (2, 2)))
+		model.add(Dropout(0.25))
 		model.add(Dense(input_dim = model.layers[-1].output_shape, units = 24, activation = 'relu'))
+		model.add(Flatten())
 		model.add(Dense(input_dim = model.layers[-1].output_shape, units = 24, activation = 'relu'))
-		model.add(Dense(input_dim = model.layers[-1].output_shape, units = nb_actions, activation = 'linear'))
-		model.compile(loss = 'mse', optimizer = Adam(lr = self.learning_rate))
+		model.add(Dense(input_dim = model.layers[-1].output_shape, units = nb_actions, activation = 'softmax'))
+		model.compile(loss = 'mse', optimizer = Adadelta())
 		return model
 
 	def get_actions(self):
@@ -51,20 +55,24 @@ class Agent:
 		self.prev_actions.append((current_state, next_state, reward, action, has_completed))
 
 	def get_action(self, current_state):
+		temp_current_state = np.expand_dims(current_state, axis = 0)
+		temp_current_state = np.expand_dims(temp_current_state, axis = 0)
 		if (np.random.rand() <= self.epsilon):
 			return random.randrange(self.nb_actions)
-		best_action = np.argmax(self.model.predict(np.array([current_state]))[0])
+		best_action = np.argmax(self.model.predict(np.array(temp_current_state))[0])
 		return best_action
 
 	def exp_replay(self, batch_size):
 		batch = random.sample(self.prev_actions, batch_size)
 		for (current_state, next_state, reward, action, has_completed) in batch:
+			temp_current_state = np.expand_dims(current_state, axis = 0)
+			temp_current_state = np.expand_dims(temp_current_state, axis = 0)
 			target = reward
 			if (has_completed):
-				target = reward + self.discount_factor * np.max(self.model.predict(np.array([current_state]))[0])
-			target_f = self.model.predict(np.array([current_state]))
+				target = reward + self.discount_factor * np.max(self.model.predict(np.array(temp_current_state))[0])
+			target_f = self.model.predict(np.array(temp_current_state))
 			target_f[0][action] = target
-			self.model.fit(np.array([current_state]), target_f, epochs = 1, verbose = 0)
+			self.model.fit(np.array(temp_current_state), target_f, epochs = 1, verbose = 0)
 		if self.epsilon > self.epsilon_min:
 			self.epsilon *= self.epsilon_decay
 
@@ -78,7 +86,7 @@ if (__name__ == '__main__'):
 	env = gym.make(ENV_NAME)
 	agent = Agent(env.observation_space.shape)
 	try:
-		agent.load_weights('first_attempt.h5')
+		agent.load_weights('second_attempt.h5')
 	except:
 		pass
 	env.close()
@@ -88,7 +96,10 @@ if (__name__ == '__main__'):
 		current_state = env.reset()
 		while True:
 			action = agent.get_action(current_state)
-			next_state, reward, has_completed, info = env.step(agent.actions[action])
+			try:
+				next_state, reward, has_completed, info = env.step(agent.actions[action])
+			except:
+				print action
 			agent.save(current_state, next_state, reward, action, has_completed)
 			current_state = next_state
 			if (has_completed):
@@ -100,4 +111,4 @@ if (__name__ == '__main__'):
 			agent.exp_replay(16)
 		env.close()
 		if (e % 5 == 0):
-			agent.save_weights('first_attempt.h5')
+			agent.save_weights('second_attempt.h5')
